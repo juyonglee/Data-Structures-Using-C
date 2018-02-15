0ff3b2476f376dd522c773c36edf1c59718b5ed6
juyonglee <juyonglee0208@gmail.com>
2018-02-16 05:27:37 +0900

    ArrayList.c - 데이터 저장 기능 추가

--- Chapter03. Linked List 1/ArrayList.c
+++ Chapter03. Linked List 1/ArrayList.c
@@ -1,10 +1,22 @@
 #include <stdio.h>
 #include "ArrayList.h"
 
-//  초기화 기능 구현
+//  [초기화 기능 구현]
 void ListInit(List *plist) {
     //  리스트에 저장된 데이터의 수를 0으로 초기화
     plist->numOfData = 0;
     //  배열의 인덱스 값으로 사용할 것이므로 0이 아닌 -1로 초기화
     plist->curPosition = -1;
+}
+
+//  [데이터 저장 기능 구현]
+void LInsert(List *plist, LData data) {
+    if(plist->numOfData == LIST_LEN) {
+        puts("저장공간이 부족해 더 이상 저장할 수 없습니다.");
+        return;
+    }
+    //  데이터 저장
+    plist->arr[plist->numOfData] = data;
+    //  저장된 데이터의 수 증가
+    (plist->numOfData)++;
 }
