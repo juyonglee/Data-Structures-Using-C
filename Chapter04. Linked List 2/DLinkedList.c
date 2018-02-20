c0d1a0924129292348a89773533268586ca1c06b
juyonglee <juyonglee0208@gmail.com>
2018-02-20 17:38:27 +0900

    정렬 기준이 없는 경우의 데이터 삽입 기능

--- Chapter04. Linked List 2/DLinkedList.c
+++ Chapter04. Linked List 2/DLinkedList.c
@@ -21,3 +21,16 @@ void LInsert(List *plist, LData data) {
         SInsert(plist, data);
     }
 }
+
+void FInsert(List* plist, LData data) {
+    //  새로운 노드 생성
+    Node *newNode = (Node*) malloc(sizeof(Node));
+    //  새로운 노드에 데이터 저장
+    newNode->data = data;
+    //  새로운 노드가 다른 노드를 가리키게 한다  
+    newNode->next = plist->head->next;
+    //  더미 노드가 새 노드를 가리키게 한다
+    plist->head->next = newNode;
+    //  저장된 노드의 수를 하나 증가
+    (plist->numOfData)++;
+}
