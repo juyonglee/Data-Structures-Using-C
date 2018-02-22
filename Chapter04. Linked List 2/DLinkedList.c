a3256a9a40fcee596e7ab1d8b3d3ee13b88eab16
juyonglee <juyonglee0208@gmail.com>
2018-02-23 01:40:48 +0900

    데이터 삽입 기능 수정

--- Chapter04. Linked List 2/DLinkedList.c
+++ Chapter04. Linked List 2/DLinkedList.c
@@ -7,19 +7,7 @@ void ListInit(List* plist) {
     plist->head = (Node*) malloc(sizeof(Node));
     plist->head->next = NULL;
     plist->comp = NULL;
-    plist->numOfData =0;
-}
-
-//  데이터 삽입 기능
-void LInsert(List *plist, LData data) {
-    //  정렬의 기준이 마련되지 않은 경우
-    if(plist->comp == NULL) {
-        //  머리에 노드를 추가
-        FInsert(plist, data);
-    } else {
-        //  정렬 기준에 따라 노드를 추가
-        SInsert(plist, data);
-    }
+    plist->numOfData = 0;
 }
 
 //  정렬 기준이 없는 경우의 데이터 삽입 기능
@@ -36,6 +24,39 @@ void FInsert(List* plist, LData data) {
     (plist->numOfData)++;
 }
 
+//  정렬 기준이 있는 경우의 데이터 삽입 기능  
+void SInsert(List* plist, LData data) {
+    //  새로운 노드 생성
+    Node* newNode = (Node*) malloc(sizeof(Node));
+    //  pred는 더미 노드를 의미
+    Node* pred = plist->head;
+    //  새로운 노드에 데이터 저장
+    newNode->data = data;
+    //  새 노드가 들어갈 위치를 찾기 위한 반복문
+    while(pred->next != NULL && plist->comp(data, pred->next->data) != 0) {
+        //  다음 노드로 이동
+        pred = pred->next;
+    }
+    //  세 노드의 뒤를 연결
+    newNode->next = pred->next;
+    //  새 노드의 앞을 연결
+    pred->next = newNode;
+    //  저장된 데이터의 수 하나 증가
+    (plist->numOfData)++;
+}
+
+//  데이터 삽입 기능
+void LInsert(List *plist, LData data) {
+    //  정렬의 기준이 마련되지 않은 경우
+    if(plist->comp == NULL) {
+        //  머리에 노드를 추가
+        FInsert(plist, data);
+    } else {
+        //  정렬 기준에 따라 노드를 추가
+        SInsert(plist, data);
+    }
+}
+
 //  첫 데이터 참조 기능
 int LFirst(List* plist, LData *pdata) {
     //  더미 노드가 NULL을 가리키는 경우
@@ -49,7 +70,7 @@ int LFirst(List* plist, LData *pdata) {
     //  cur은 첫 번째 노드를 가르킨다
     plist->cur = plist->head->next;
     //  첫 번째 노드의 데이터를 전달
-    *pdata = plist->head->next->data;
+    *pdata = plist->cur->data;
     //  데이터 반환 성공
     return TRUE;
 }
@@ -57,7 +78,7 @@ int LFirst(List* plist, LData *pdata) {
 //  두 번째 이후 데이터 참조 기능
 int LNext(List* plist, LData *pdata) {
     //  더미 노드가 NULL을 가리키는 경우
-    if(plist->cur == NULL)
+    if(plist->cur->next == NULL)
         return FALSE;
     //  cur이 가리키던 것을 before가 가리킴
     plist->before = plist->cur;
@@ -91,3 +112,6 @@ int LCount(List* plist) {
     return plist->numOfData;
 }
 
+void SetSortRule(List* plist, int (*comp)(LData d1, LData d2)) {
+    plist->comp = comp;
+}
