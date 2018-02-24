bac292c2978a02bdf436ad8cdc6ebe0a83a1a60f
juyonglee <juyonglee0208@gmail.com>
2018-02-25 01:19:19 +0900

    CLinkedList.c - 머리에 새로운 노드를 추가하는 기능 추가

--- Chapter05. Linked List 3/CLinkedList.c
+++ Chapter05. Linked List 3/CLinkedList.c
@@ -9,3 +9,25 @@ void ListInit(List* plist) {
     plist->before = NULL;
     plist->numOfData = 0;
 }
+
+//  새로운 노드를 머리에 추가하는 기능
+void LInsertFront(List* plist, Data data) {
+    //  새로운 노드 생성
+    Node* newNode = (Node*) malloc(sizeof(Node));
+    //  새로운 노드에 데이터 저장
+    newNode->data = data;
+
+    //  첫 번째 노드인 경우
+    if(plist->tail == NULL) {
+        //  tail이 새로만든 노드를 가리키게 한다.
+        plist->tail == newNode;
+        //  새로운 노드가 자신을 가리키게 한다.
+        newNode->next = newNode;
+    } else {
+        //  새로운 노드를 추가
+        newNode->next = plist->tail->next;
+        //  마지막 노드와 새로운 노드를 연결
+        plist->tail->next = newNode;
+    }
+    (plist->numOfData)++;
+}
