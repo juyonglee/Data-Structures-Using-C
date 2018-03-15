76080769905975a21160cc4d6848fbe4bbabec86
juyonglee <juyonglee0208@gmail.com>
2018-03-15 21:58:35 +0900

    후위 표기법의 수식을 계산하는 구현 파일 구현

--- Chapter06. Stack/PostCalculator.c
+++ Chapter06. Stack/PostCalculator.c
@@ -3,5 +3,35 @@
 #include "ListBaseStack.h"
 
 int EvalRPNexp(char exp[]) {
-       
+    Stack stack;
+    int expLen = strlen(exp);
+    
+    StackInit(&stack);
+    char tok, op1, op2;
+
+    for(int i=0; i<expLen; i++) {
+        tok = exp[i];
+        if(isdigit(tok)) {
+            SPush(&stack, tok-'0');            
+        } else {
+            op2 = SPop(&stack);
+            op1 = SPop(&stack);
+
+            switch(tok){
+                case '+':
+                    SPush(&stack, op1+op2);
+                    break;
+                case '-':
+                    SPush(&stack, op1-op2);
+                    break;
+                case '*':
+                    SPush(&stack, op1*op2);
+                    break;
+                case '/':
+                    SPush(&stack, op1/op2);
+                    break;
+            }
+        }
+    }
+    return SPop(&stack);
 }
