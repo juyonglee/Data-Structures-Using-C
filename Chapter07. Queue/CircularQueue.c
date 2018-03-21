692cbb975833e85ea19521dadf3003065c6445cb
juyonglee <juyonglee0208@gmail.com>
2018-03-21 20:00:23 +0900

    CircularQueue.c - 큐가 비어있는지 확인하는 기능 추가

--- Chapter07. Queue/CircularQueue.c
+++ Chapter07. Queue/CircularQueue.c
@@ -2,7 +2,15 @@
 #include <stdlib.h>
 #include "CircularQueue.h"
 
+//  Queue를 초기화하는 기능
 void QueueInit(Queue *pq) {
     pq->front = 0;
     pq->rear = 0;
+}
+
+//  Queue가 비어있는지 확인하는 기능
+int QIsEmpty(Queue *pq) {
+    if(pq->front == pq->rear)
+        return TRUE;
+    return FALSE;
 }
