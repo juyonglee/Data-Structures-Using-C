8a3d0add140894067c0be733d5b8692dd9cc1b7b
juyonglee <juyonglee0208@gmail.com>
2018-03-27 21:56:13 +0900

    ListBaseQueue.c - 큐가 비어있는지 확인하는 기능 추가

--- Chapter07. Queue/ListBaseQueue.c
+++ Chapter07. Queue/ListBaseQueue.c
@@ -7,3 +7,10 @@ void QueueInit(Queue* pq) {
     pq->front = NULL;
     pq->rear = NULL;
 }
+
+//  Queue가 비어있는지 확인하는 기능
+int QIsEmpty(Queue* pq) {
+    if(pq->front == NULL)
+        return TRUE;
+    return FALSE;
+}
