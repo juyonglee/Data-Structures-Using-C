dea09a909badd7529cd74cd39ce5486f6075b29e
juyonglee <juyonglee0208@gmail.com>
2018-04-06 16:45:36 +0900

    Deque.c - 덱이 비어있는지 확인하는 기능 추가

--- Chapter07. Queue/Deque.c
+++ Chapter07. Queue/Deque.c
@@ -2,8 +2,16 @@
 #include <stdlib.h>
 #include "Deque.h"
 
+//  Dequeue를 초기화하는 기능
 void DequeInit(Deque * pdeq)
 {
 	pdeq->head = NULL;
 	pdeq->tail = NULL;
+}
+
+//  Dequeue가 비어있는지 확인하는 기능
+int DQIsEmpty(Deque * pdeq) {
+    if(pdeq->head == NULL)
+        return TRUE;
+    return FALSE;
 }
